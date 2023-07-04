0f2a0e37fe2eaa74a78410b5882cf6c7331909d7
ChienHM <chienhm@apero.vn>
2023-07-04 18:21:04 +0700

    Update

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -10,16 +10,15 @@ public class Loading : MonoBehaviour
     public static Loading Instance { get; private set; }
     [SerializeField] Image fillImage;
     [SerializeField] GameObject LoadingPopup;
-    [SerializeField] float _maxTimeLoading = 30f;
+    [SerializeField] float _maxTimeLoading = 0f;
     [SerializeField] int _numberCondition = 0;
-    [SerializeField]
-    List<bool> _conditionDone = new List<bool>();
-
+    [SerializeField]List<bool>_conditionDone = new List<bool>();
 
     Action<List<bool>> _onDone = null;
-
     float _loadingMaxvalue;
     bool _isLoadingStart = false;
+
+    
 
     private void Awake()
     {
@@ -28,42 +27,32 @@ public class Loading : MonoBehaviour
         fillImage.fillAmount = 0;
         
     }
-    private void Start()
+    
+    public Loading Init(Action<List<bool>> onDone = null)
     {
-        StartCoroutine(IELoad(5));
-        SceneManager.LoadSceneAsync(1);
-
-
-
-    }
-
-    //public Loading Init(Action<List<bool>>oneDone = null)
-    //{
-    //    _conditionDone.Clear();
-    //    for(int i=0;i< _numberCondition; i++)
-    //    {
-    //        _conditionDone.Add(false);
-    //    }    
-    //}
-    //public Loading Init(int numberCondition, Action<List<bool>> onDone = null)
-    //{
-    //    _numberCondition = numberCondition;
-    //    return Init(onDone);
-    //}
-    public Loading SetMaxTimeLoading(float maxTime)
-    {
-        _maxTimeLoading = maxTime;
-        return this;
-    }
-    private IEnumerator IELoad(float time)
-    {
-        float deltatime = 0f;
-        while(deltatime < time)
+        for(int i=0;i<_numberCondition;i++)
         {
-            deltatime += Time.deltaTime;
-            yield return null;
-            fillImage.fillAmount = deltatime/time;
+            _conditionDone.Add(false);
         }
-        this.gameObject.SetActive(false);
+        _loadingMaxvalue = 1f;
+        fillImage.fillAmount = 0f;
+
+
+
+        LoadingPopup.SetActive(true);
+        _isLoadingStart = true;
+        return this;
     }    
+
+    public Loading Init(int numberCondition,Action<List<bool>>onDone = null)
+    {
+        _numberCondition = numberCondition;
+        return Init(onDone);
+    }
+    private void Update()
+    {
+        //fillImage.fillAmount +=
+    }
+
+
 }
